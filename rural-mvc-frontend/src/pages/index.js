ebe482a267355dcfce03f6d8a76f93dd4c6b6e64
Gabriela Barrence <gabriela.barrence@gmail.com>
2023-04-23 17:32:32 -0300

    testing the new branch

--- rural-mvc-frontend/src/pages/index.js
+++ rural-mvc-frontend/src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <button onClick={teste}>BOTAOZINHO</button>
+        <button onClick={teste}>BOTAOZAO</button>
         <div className={styles.description}>
           <p>
             GABRIEL PIRES&nbsp;
